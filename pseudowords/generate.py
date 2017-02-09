bc87ed090457db428f12e6d4b6338dc2502c11f5
Sébastien Lerique <sl@mehho.net>
2017-02-10 00:03:28 +0100

    feat/pseudowords: make into a final usable cli
    Export to csv is finished.

--- pseudowords/generate.py
+++ pseudowords/generate.py
@@ -1,6 +1,8 @@
 import re
-import sys
 import os
+import argparse
+import itertools
+import csv
 # The readline module works with `input` by simply importint it, so we disable
 # linting with flake8 for this line
 import readline  # noqa
@@ -117,38 +119,86 @@ def test_text_targets(nlp):
          ('brains', (69, 75))]
 
 
+argparser = argparse.ArgumentParser(description='Generate jabberwocky '
+                                    'sentences from reference sentences')
+argparser.add_argument('sentences', metavar='SENTENCE', nargs='*',
+                       help='sentences to use as reference. Required if no '
+                       'reference file is provided through --reference '
+                       '(added to sentences in the --reference file '
+                       'otherwise).')
+argparser.add_argument('--reference', metavar='REFERENCE_FILE',
+                       type=argparse.FileType(), default=[],
+                       help='a file containing reference sentences, '
+                       'one per line (empty lines are ignored). Required '
+                       'if no sentences are provided on the command line.')
+argparser.add_argument('--out', metavar='OUT_FILE',
+                       type=argparse.FileType('w'), default=None,
+                       help='a file to output generated sentences to, '
+                       'in csv format, with suggested pseudowords. '
+                       'If not provided, output is printed to stdout.')
+
+
+def print_status(message, out):
+    if out is None:
+        # We're printing generated results to stdout, so we add some formatting
+        # so as not to pollute the output
+        message = '\x1b[2K\r' + bcolors.LOW + message + bcolors.ENDC
+    print(message, end='' if out is None else None)
+
+
+def append(current, part, out, format=''):
+    if out is None:
+        # We're printing generated results to stdout, so we add some colours
+        part = format + part + bcolors.ENDC
+    current.append(part)
+
+
 if __name__ == '__main__':
-    # Use by giving your input sentences as arguments:
-    # python pseudowords/generate.py "This is a sentence." "And another."
+    args = argparser.parse_args()
+    outwriter = csv.writer(args.out) if args.out is not None else None
+
+    # Check we got at least one sentence
+    if len(args.sentences) == 0 and args.reference == []:
+        argparser.print_help()
+        quit()
 
     # Load spaCy
-    print(bcolors.LOW + 'Loading spaCy' + bcolors.ENDC)
+    print_status('Loading spaCy...', args.out)
     nlp = spacy.load('en')
     test_text_targets(nlp)
 
     # Load Wuggy
-    print(bcolors.LOW + 'Loading Wuggy' + bcolors.ENDC)
+    print_status('Loading Wuggy...', args.out)
     wuggy = Generator()
     wuggy_path = os.path.abspath(os.path.dirname(generator.__file__))
     wuggy.data_path = os.path.join(wuggy_path, 'data')
     wuggy.load(orthographic_english)
     wuggy_options = wuggy.default_options()
     pseudo_count = 10
+    print_status('', args.out)
 
-    for line in sys.argv[1:]:
-        line = normalize(line)
-        doc = nlp(line)
+    for text in itertools.chain(args.sentences, args.reference):
+        text = normalize(text)
+        if len(text) == 0:
+            continue
+
+        doc = nlp(text)
         targets = text_targets(doc)
 
         # Generate all pseudowords
-        print(bcolors.LOW + 'Generating pseudowords' + bcolors.ENDC)
         all_pseudowords = []
         for target in targets:
-            segments = wuggy.lookup(target.orth_.lower()).lower()
-            if segments is None:
+            segments = wuggy.lookup(target.orth_.lower())
+
+            while segments is None:
                 segments = input("Couldn't determine a segmentation for '" +
                                  bcolors.BOLD + target.orth_ + bcolors.ENDC +
                                  "', please enter one yourself: ").lower()
+                if segments.replace('-', '').lower() != target.orth_.lower():
+                    print("Your entry doesn't match the target word when "
+                          "converting back. Please try again.")
+                    segments = None
+
             target_pseudowords = \
                 [apply_shape(target.orth_, w)
                  for (_, w) in wuggy.run(wuggy_options, segments, '')]
@@ -157,26 +207,43 @@ if __name__ == '__main__':
                                    (pseudo_count - len(target_pseudowords)))
             all_pseudowords.append(target_pseudowords)
 
-        # Print the actual sentence with target words highlighted
+        # Collect sentence parts, with colours if we're printing to stdout
         last_end = 0
+        line = []
         for target in targets:
             start, end = span(target)
-            assert target.orth_ == line[start:end]
-            print(line[last_end:start], end='')
-            print(bcolors.BOLD + bcolors.OKGREEN + target.orth_ + bcolors.ENDC,
-                  end='')
+            assert target.orth_ == text[start:end]
+            append(line, text[last_end:start], args.out)
+            append(line, target.orth_, args.out,
+                   bcolors.BOLD + bcolors.OKGREEN)
             last_end = end
-        print(line[last_end:])
+        append(line, text[last_end:], args.out)
 
-        # Print candidate pseudowords for each target
+        # Actually print to stdout or to the out file
+        if args.out is None:
+            print(''.join(line))
+        else:
+            outwriter.writerow(line)
+
+        # Collect candidate pseudowords for each target
         for i in range(pseudo_count):
             pseudowords = [target_pseudowords[i]
                            for target_pseudowords in all_pseudowords]
             last_end = 0
+            line = []
             for target, pseudoword in zip(targets, pseudowords):
                 start, end = span(target)
                 assert end - start == len(pseudoword)
-                print(' ' * (start - last_end), end='')
-                print(bcolors.OKBLUE + pseudoword + bcolors.ENDC, end='')
+                append(line, ' ' * (start - last_end), args.out)
+                append(line, pseudoword, args.out, bcolors.OKBLUE)
                 last_end = end
-            print()
+            append(line, '', args.out)
+
+            # Actually print to stdout or to the out file
+            if args.out is None:
+                print(''.join(line))
+            else:
+                outwriter.writerow(line)
+
+    if args.out is not None:
+        print('All done!')
