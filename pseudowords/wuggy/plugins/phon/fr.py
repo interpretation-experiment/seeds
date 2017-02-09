5dce3006b8affe44f9f1d9bcf337cfda180de072
Sébastien Lerique <sl@mehho.net>
2017-02-09 15:58:19 +0100

    fix/wuggy: fix character encoding

--- pseudowords/wuggy/plugins/phon/fr.py
+++ pseudowords/wuggy/plugins/phon/fr.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2009-2012 Emmanuel Keuleers <emmanuel.keuleers@ugent.be>
 # Refactored parts of Wuggy 0.2.2b2 <http://crr.ugent.be/Wuggy>, adapted by
-# Sébastien Lerique <sl@mehho.net>
+# SÃ©bastien Lerique <sl@mehho.net>
 
 
 import re
 
 
-single_vowels = ['a', 'i', 'y', 'u', 'o', 'O', 'e', 'E', '°', '2', '9', '5',
-                 '1', '@', '§', '3']
+single_vowels = ['a', 'i', 'y', 'u', 'o', 'O', 'e', 'E', 'Â°', '2', '9', '5',
+                 '1', '@', 'Â§', '3']
 nucleuspattern = '{}'.format(single_vowels)
 oncpattern = re.compile('(.*?)({})(.*)'.format(nucleuspattern))
